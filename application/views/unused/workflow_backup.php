84b0d8edcd05f564949407d33e392c60e6cab436
MasonMcGough <mcgough.mason@gmail.com>
2017-02-03 21:55:17 -0500

    Added unused files to tracking;

---
+++ application/views/unused/workflow_backup.php
@@ -0,0 +1,292 @@
+<!doctype html>
+<html>
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+	
+	<title>Workflow | CytoInformatics</title>
+	
+	<!-- FAVICON AND APPLE TOUCH -->    
+	<link rel="shortcut icon" href="favicon.png">
+	<link rel="apple-touch-icon-precomposed" sizes="180x180" href="apple-touch-180x180.png">
+	<meta name="msapplication-TileImage" content="mstile.png">
+	
+	<!-- FONTS -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+	
+	<!-- BOOTSTRAP CSS -->
+	<link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css'); ?>"> 
+	
+	<!-- FONT AWESOME -->
+	<link rel="stylesheet" href="<?php echo base_url('assets/fonts/fontawesome/css/font-awesome.min.css'); ?>">
+	
+	<!-- BRONX ICONS -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/fonts/bronx-icons/css/bronx-icons.min.css'); ?>">
+	
+	<!-- FANCYBOX -->
+	<link rel="stylesheet" href="<?php echo base_url('assets/plugins/fancybox/jquery.fancybox.css'); ?>">
+	
+	<!-- REVOLUTION SLIDER -->
+	<link rel="stylesheet" href="<?php echo base_url('assets/plugins/revolutionslider/css/settings.css'); ?>">
+	<link rel="stylesheet" href="<?php echo base_url('assets/plugins/revolutionslider/css/layers.css'); ?>">
+	<link rel="stylesheet" href="<?php echo base_url('assets/plugins/revolutionslider/css/navigation.css'); ?>">
+	
+	<!-- OWL Carousel -->
+	<link rel="stylesheet" href="<?php echo base_url('assets/plugins/owl-carousel/owl.carousel.css'); ?>">
+	
+	<!-- YOUTUBE PLAYER -->
+	<link rel="stylesheet" href="<?php echo base_url('assets/plugins/ytplayer/css/jquery.mb.ytplayer.min.css'); ?>">
+	
+	<!-- ANIMATIONS -->
+	<link rel="stylesheet" href="<?php echo base_url('assets/plugins/animations/animate.min.css'); ?>">
+	
+	<!-- CUSTOM & PAGES STYLE -->
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/custom.css'); ?>">
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/pages-style.css'); ?>">
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/my-styles.css'); ?>">
+	
+
+</head>
+
+<body class="header-boxed">
+
+	<div id="main-container">
+		
+		<!-- HEADER -->
+		<header id="header-container">
+			<!-- NAVIGATION BAR -->
+			<?php include "application/templates/navigation_bar.php"; ?>
+			
+			<div id="page-header" class="parallax" data-stellar-background-ratio="0.1" style="background-image:url(images/backgrounds/page-header-1.jpg);">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12">
+							
+							<h1>Workflow</h1>
+							
+							<p class="tp-caption text-center text">View a quick explanation of our services</p>
+							
+						</div><!-- col -->
+					</div><!-- row -->
+				</div><!-- container -->    
+			</div><!-- page-header -->
+            
+		</header><!-- HEADER CONTAINER -->
+		
+		
+		<!-- PAGE CONTENT -->
+		<div id="page-content">
+            <!--<div class="container">
+                <div class="text-center">
+                    <h1>Under Construction</h1>
+                    <h4>Please come back soon!</h4>
+                    <p><a href="<?php echo base_url('index.php/cyto_bioformatics/index'); ?>">Return to home page</a></p>
+                </div>
+            </div>-->
+                
+            <div id="workflow-slideshow" class="container">
+                <div id="workflow-slide-1">
+                    <div class="text-center short-margin">
+                        <h2>Say you are a pathologist...</h2>
+                    </div>
+
+                    <div class="col-lg-offset-1 text-block short-margin">
+                        <p>You have several hundred images of lung biopsies. You know that some of your images depict tissue with signs of small cell carcinoma, some show large cell carcinoma, and some show no signs of carcinoma. </p>
+                    </div>
+                </div>
+                    
+                <div id="workflow-slide-2">
+                    <div class="text-center short-margin">
+                        <h4>But you are also a researcher.</h4>
+                    </div>
+
+                    <div class="col-lg-offset-1 text-block short-margin">
+                        <p>You want to publish high-quality studies with impact. You know that there are significant morphological differences between the tissue samples you have. However, you don't have the time or resources to pay an assistant to manually annotate each image by hand, much less do it yourself. If only you could find a way to easily quantify these differences!</p>
+                    </div>
+                </div>
+                
+                
+                <div id="workflow-slide-3">
+                    <div class="text-center short-margin">
+                        <h3>That's where <span class="text-alt-color">CytoInformatics</span> comes in.</h3>
+                    </div>
+
+                    <div class="col-lg-offset-1 text-block short-margin">
+                        <p>Here at CytoInformatics, we aim to simplify the analysis of microscopy images for you while delivering pinpoint-accurate results.</p>
+
+                        <p>To get started, follow these simple instructions.</p>
+                    </div>
+                </div>
+                
+                
+                <div id="workflow-slide-4">
+                    <div class="text-center short-margin">
+                        <h4>Step 1: Submit a Quote</h4>
+                    </div>
+
+                    <div class="col-lg-offset-1 text-block short-margin">
+                        <p>Click the <a href="<?php echo base_url('index.php/cyto_bioformatics/quote'); ?>">Quote</a> link at the top to access our quote submission form. You do not even need to register for an account to submit a quote. Just provide us with a brief description of the task you are looking to solve. We will contact you to discuss the problem you are looking to solve.</p> 
+
+                        <p>You are also encouraged to submit up to three sample images so that we may better prepare to discuss with you.</p>
+                    </div>
+                </div>
+            </div>
+		</div><!-- PAGE CONTENT -->
+		
+		<!-- FOOTER -->
+		<?php include 'application/templates/footer.php'; ?>
+	</div><!-- MAIN CONTAINER -->
+	
+	
+	<!-- SCROLL UP -->
+	<a id="scroll-up"><i class="fa fa-angle-up"></i></a>
+	
+	
+	<!-- jQUERY -->
+	<script src="<?php echo base_url('assets/plugins/jquery/jquery-2.2.2.min.js'); ?>"></script>
+	
+	<!-- BOOTSTRAP JS -->
+	<script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js'); ?>"></script>
+	
+	<!-- VIEWPORT -->
+	<script src="<?php echo base_url('assets/plugins/viewport/jquery.viewport.js'); ?>"></script>
+	
+	<!-- MENU -->
+	<script src="<?php echo base_url('assets/plugins/menu/hoverIntent.js'); ?>"></script>
+	<script src="<?php echo base_url('assets/plugins/menu/superfish.js'); ?>"></script>
+	
+	<!-- FANCYBOX -->
+	<script src="<?php echo base_url('assets/plugins/fancybox/jquery.fancybox.pack.js'); ?>"></script>
+
+	<!-- PARALLAX -->
+	<script src="<?php echo base_url('assets/plugins/parallax/jquery.stellar.min.js'); ?>"></script>
+	
+	<!-- ISOTOPE -->
+	<script src="<?php echo base_url('assets/plugins/isotope/imagesloaded.pkgd.min.js'); ?>"></script>
+	<script src="<?php echo base_url('assets/plugins/isotope/isotope.pkgd.min.js'); ?>"></script>
+	
+	<!-- PLACEHOLDER -->
+	<script src="<?php echo base_url('assets/plugins/placeholders/jquery.placeholder.min.js'); ?>"></script>
+	
+	<!-- CONTACT FORM VALIDATE & SUBMIT -->
+	<script src="<?php echo base_url('assets/plugins/validate/jquery.validate.min.js'); ?>"></script>
+	<script src="<?php echo base_url('assets/plugins/submit/jquery.form.min.js'); ?>"></script>
+
+	<!-- CHARTS -->
+	<script src="<?php echo base_url('assets/plugins/charts/jquery.easypiechart.min.js'); ?>"></script>
+	
+	<!-- COUNTER -->
+	<script src="<?php echo base_url('assets/plugins/counter/jquerysimplecounter.js'); ?>"></script>
+	
+	<!-- STATISTICS -->
+	<script src="<?php echo base_url('assets/plugins/statistics/chart.min.js'); ?>"></script>
+	
+	<!-- INSTAFEED -->
+	<script src="<?php echo base_url('assets/plugins/instafeed/instafeed.min.js'); ?>"></script>
+	
+	<!-- COUNTDOWN -->
+	<script src="<?php echo base_url('assets/plugins/countdown/jquery.countdown.min.js'); ?>"></script>
+	
+	<!-- ANIMATIONS -->
+	<script src="<?php echo base_url('assets/plugins/animations/wow.min.js'); ?>"></script>
+	
+	<!-- CUSTOM JS -->
+	<script src="<?php echo base_url('assets/js/custom.js'); ?>"></script>
+    
+    <!-- ANIMATIONS -->
+    <script src="<?php echo base_url('assets/js/velocity.min.js'); ?>"></script> 
+    
+    <script text="type/javascript">
+        $(document).ready(function() {
+            document.getElementById("workflow-button").setAttribute("class", "active");
+        });
+        
+//        function randnum(min, max) {
+//            return Math.floor(Math.random()*(max-min+1)+min);
+//        }
+//        
+//        var dotsCount = 50,
+//            dotsHtml = "",
+//            $count = $("#count"),
+//            $dots;
+//        
+//        for (var i = 0; i < dotsCount; i++) {
+//            dotsHtml += "<div class='dot'></div>";
+//        }
+//        
+//        $dots = $(dotsHtml);
+//        $count.html(dotsCount);
+//        
+//        var $container = $("#container"),
+//		    $browserWidthNotice = $("#browserWidthNotice");
+//
+//        var screenWidth = window.screen.availWidth,
+//		    screenHeight = window.screen.availHeight,
+//		    chromeHeight = screenHeight - (document.documentElement.clientHeight || screenHeight);
+//
+//	    var translateZMin = -725,
+//		    translateZMax = 600;
+//
+//	    var containerAnimationMap = {
+//			perspective: [ 215, 50 ],
+//			opacity: [ 0.90, 0.55 ]
+//		};
+//
+//        /* IE10+ produce odd glitching issues when you rotateZ on a parent element subjected to 3D transforms. */
+//        if (!isIE) {
+//            containerAnimationMap.rotateZ = [ 5, 0 ];
+//        }
+//
+//        /* Ensure the user is full-screened; this demo's translations are relative to screen width, not window width. */
+//        if ((document.documentElement.clientWidth / screenWidth) < 0.80) {
+//            $browserWidthNotice.show();
+//        }
+//
+//        /*****************
+//            Animation
+//        *****************/
+//
+//        /* Animate the dots' container. */
+//        $container
+//            .css("perspective-origin", screenWidth/2 + "px " + ((screenHeight * 0.45) - chromeHeight) + "px")
+//            .velocity(containerAnimationMap, { duration: 800, loop: 1, delay: 3250 });
+//
+//        /* Special visual enhancement for WebKit browsers, which are faster at box-shadow manipulation. */
+//        if (isWebkit) {
+//            $dots.css("boxShadow", "0px 0px 4px 0px #4bc2f1");
+//        }
+//
+//        /* Animate the dots. */
+//        $dots
+//            .velocity({ 
+//                translateX: [ 
+//                    function() { return "+=" + randnum(-screenWidth/2.5, screenWidth/2.5) },
+//                    function() { return randnum(0, screenWidth) }
+//                ],
+//                translateY: [
+//                    function() { return "+=" + randnum(-screenHeight/2.75, screenHeight/2.75) },
+//                    function() { return randnum(0, screenHeight) }
+//                ],
+//                translateZ: [
+//                    function() { return "+=" + randnum(translateZMin, translateZMax) },
+//                    function() { return randnum(translateZMin, translateZMax) }
+//                ],
+//                opacity: [ 
+//                    function() { return Math.random() },
+//                    function() { return Math.random() + 0.1 }
+//                ]
+//            }, { duration: 6000 })
+//            .velocity("reverse", { easing: "easeOutQuad" })
+//            .appendTo($container);
+        
+    </script>
+	
+</body>
+
+</html>
---
+++ application/views/unused/youtube_sample.php
@@ -0,0 +1,23 @@
+<!-- YOUTUBE VIDEO
+<a class="youtube-player" data-property="{videoURL:'https://www.youtube.com/watch?v=OO-Bc8s50F4',containment:'#youtube-video-section',showControls:true,autoPlay:true, mute:true, startAt:0, opacity:1}">My video</a>
+
+<section class="full-section dark-section" id="video-section">
+
+    <div id="youtube-video-section">
+        <div class="full-section-overlay-color"></div>
+
+    <div class="full-section-container">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+
+                    <h2 class="text-center text-default-color">Watch our event live</h2>
+                    <h5 class="text-center">A multaitude of ideas in a one day seminary</h5>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>-->
