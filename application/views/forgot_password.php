255bdc05d12cb6a623ed6d3aeb634d5e59f3e387
MasonMcGough <mcgough.mason@gmail.com>
2017-02-14 10:02:06 -0500

    Added favicons to all major views and removed old favicons; Added -Return to Login- button to forgot_password.php; Autofilled fields in quote.php when user has _SESSION data; Allowed fields in quote that are autofilled by _SESSION to animate at document ready;

--- application/views/forgot_password.php
+++ application/views/forgot_password.php
@@ -3,6 +3,26 @@
 <head>
   <meta charset="UTF-8">
   <title>Forgot Password | CytoInformatics</title>
+    
+  <!-- FAVICON AND APPLE TOUCH -->    
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo base_url('images/favicon/apple-icon-57x57.png') ?>">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo base_url('images/favicon/apple-icon-60x60.png') ?>">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo base_url('images/favicon/apple-icon-72x72.png') ?>">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url('images/favicon/apple-icon-76x76.png') ?>">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo base_url('images/favicon/apple-icon-114x114.png') ?>">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo base_url('images/favicon/apple-icon-120x120.png') ?>">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo base_url('images/favicon/apple-icon-144x144.png') ?>">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo base_url('images/favicon/apple-icon-152x152.png') ?>">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url('images/favicon/apple-icon-180x180.png') ?>">
+    <link rel="icon" type="image/png" sizes="192x192" href="<?php echo base_url('images/favicon/android-icon-192x192.png') ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo base_url('images/favicon/favicon-32x32.png') ?>">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo base_url('images/favicon/favicon-96x96.png') ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url('images/favicon/favicon-16x16.png') ?>">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="<?php echo base_url('images/favicon/ms-icon-144x144.png') ?>">
+    <meta name="theme-color" content="#ffffff">
+    
   <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
   <link rel="stylesheet" href="<?php echo base_url('assets/css/login.css'); ?>">
@@ -33,6 +53,8 @@
               </label>
               <input type="email" name="email" required autocomplete="off"/>
             </div>
+              
+            <p class="forgot"><a href="<?php echo base_url('index.php/cyto_bioformatics/login'); ?>">Return to Login</a></p>
           
             <button class="button button-block">Send Email</button>
           </form>
